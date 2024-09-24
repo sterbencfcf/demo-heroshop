0326a9ddc6bbb381e93e27b8903d96ec8c42066e
sterbencf <2314581726@qq.com>
2024-09-24 22:29:39 +0800

    9.24新建项目

---
+++ src/main/java/com/hero/heroshop/HeroshopApplication.java
@@ -0,0 +1,13 @@
+package com.hero.heroshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hero/heroshop/HeroshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hero.heroshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
